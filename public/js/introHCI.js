f63c266e7eb12f1e09348db81649e99c0c218fd7
biancamoy <56455560+biancamoy@users.noreply.github.com>
2020-02-13 13:57:46 -0800

    Update introHCI.js

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -20,11 +20,21 @@ function initializePage() {
 function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
-
+	$.get
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	console.log("Trying to get data from: "+"/project/"+idNumber);
+	$.get("/project/" + idNumber, function (result) {
+		console.log("#project" + idNumber + ".details");
+		var projectHTML = '<p>' + result['title'] + '</p>' +
+		'<p>' + result['date'] + '</p>' +
+		'<img src="' + result['image'] + '" class = "detailsImage">' +
+		'</p>' + result['summary'] + '</p>';
+		$("#project" + idNumber + " .details".html(projectHTML);
+	});
 }
